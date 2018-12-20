71f0278556b558c51a932a2d886c5de5da743642
tim <timscode1@gmail.com>
2018-12-20 18:17:53 -0500

    params on index work now

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,16 +3,14 @@ class HomeController < ApplicationController
  	@proteins = Protein.all
  	@sauces = Sauce.all
  	@wines = Wine.all
+   if params[:protein] != nil
+     @protein = Protein.find(params[:protein]).name
+     @sauce = Sauce.find(params[:sauce]).name
+   end
+ 
   end
 
   def about
   end
-
-  def submit
-	@wines = Wine.all
-	@protein = Protein.find(params[:protein]).name	
-	@sauce = Sauce.find(params[:sauce]).name
-	redirect_to root_path
-	end 
 
 end
