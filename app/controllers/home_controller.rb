710e90ba2e020dad60fda9f69c5a019915d56cac
tim <timscode1@gmail.com>
2018-12-20 16:20:46 -0500

    route probs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,16 @@ class HomeController < ApplicationController
   def index
  	@proteins = Protein.all
  	@sauces = Sauce.all
+ 	@wines = Wine.all
   end
 
   def about
   end
+
+  def submit
+	@protein = params[:protein]
+	@sauce = params[:sauce]
+	redirect_to root_path
+	end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'home/index'
   get 'home/about'
+  post 'hub/submit' => 'hub#submit'
   root 'home#index'
-  post 'submit' => 'hub#submit'
 end
